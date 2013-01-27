28763ae8f2f6e54d052d9999b0747f233f7b7cd2
Jon Waldow <jwaldow@mines.edu>
2013-01-26 19:45:52 -0700

    adding finished albums.rb and start of the form.html:

--- project02/albums.rb
+++ project02/albums.rb
@@ -1,9 +1,36 @@
+#!/usr/bin/env/ ruby
 require 'rack'
 
-class HelloWorld
-  def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
-  end
+class AlbumApp
+	def call(env)
+		request = Rack::Request.new(env)
+		case request.path
+		when "/form" then render_form(request)
+		when "/list" then render_list(request)
+		else render_404
+		end
+	end
+
+	def render_form(request)
+		response = Rack::Response.new
+		File.open("form.html", "rb"){ |form| response.write(form.read)}
+		response.finish
+	end
+
+	def render_list(request)
+		response = Rack::Response.new(request.path)
+		response.finish
+	end
+
+	def render_404
+		[404, {"Content-Type" => "text/plain"}, ["Nothing here!"]]
+	end
 end
+
+# class HelloWorld
+#   def call(env)
+#     [200, {"Content-Type" => "text/plain"}, ["Hello from Rack!"]]
+#   end
+# end
 
 Rack::Handler::WEBrick.run HelloWorld.new, :Port => 8080
