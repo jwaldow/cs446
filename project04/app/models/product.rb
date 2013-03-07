8cc087e9cdec44fd40662e5bc11d7ddd67e3915c
Jon Waldow <jwaldow@mines.edu>
2013-03-07 10:44:44 -0700

    fixed iterator and made product assesible

--- project04/app/models/product.rb
+++ project04/app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  attr_accessible :description, :image_url, :price, :title
+  attr_accessible :description, :image_url, :price, :title, :product
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
